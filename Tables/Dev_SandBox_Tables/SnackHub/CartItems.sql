2f702546736b3a81a96e551a1b46273753a5914a
João Victor <joaovicto2077@gmail.com>
2025-03-17 22:05:33 -0300

    Create CartItems.sql

---
+++ Tables/Dev_SandBox_Tables/SnackHub/CartItems.sql
@@ -0,0 +1,25 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[CartItems](
+	[CartItemId] [int] IDENTITY(1,1) NOT NULL,
+	[ProductQuantity] [int] NOT NULL,
+	[ShoppingCartId] [nvarchar](200) NOT NULL,
+	[ProductId] [int] NOT NULL,
+ CONSTRAINT [PK_CartItems] PRIMARY KEY CLUSTERED 
+(
+	[CartItemId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[CartItems]  WITH CHECK ADD  CONSTRAINT [FK_CartItems_Products_ProductId] FOREIGN KEY([ProductId])
+REFERENCES [dbo].[Products] ([ProductId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[CartItems] CHECK CONSTRAINT [FK_CartItems_Products_ProductId]
+GO
