219e26eb08a9e3c2c07ec8944b77a6e662fc7fab
João Victor <joaovicto2077@gmail.com>
2025-03-19 14:33:55 -0300

    Update Categories.sql

--- Tables/Dev_SandBox_Tables/SnackHub/Categories.sql
+++ Tables/Dev_SandBox_Tables/SnackHub/Categories.sql
@@ -8,11 +8,15 @@ CREATE TABLE [dbo].[Categories](
 	[CategoryId] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](50) NOT NULL,
 	[Description] [nvarchar](500) NOT NULL,
-	[CreationDate] [datetime2](7) NULL,
-	[UpdateDate] [datetime2](7) NULL,
+	[Status] [int] NOT NULL,
+	[CreationDate] [datetime2](0) NULL,
+	[UpdateDate] [datetime2](0) NULL,
  CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
 (
 	[CategoryId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Categories] ADD  DEFAULT ((0)) FOR [Status]
 GO
